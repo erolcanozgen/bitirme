7ca59bca0c46d7676041ef1034365f7490dce86d
Erol Can OZGEN <erolcanozgen@gmail.com>
2013-12-10 19:04:53 +0200

    Registration formunun register butonunun click methodu yazıldı.

--- App_Code/Users.cs
+++ App_Code/Users.cs
@@ -15,6 +15,7 @@ public class Users
     public string name;
     public string surname;
     public string email;
+    public string passwd;
     public int rolId;
 	public Users()
 	{
@@ -24,7 +25,18 @@ public class Users
         this.name = String.Empty;
         this.surname = String.Empty;
         this.email = String.Empty;
+        this.passwd = String.Empty;
 	}
+
+    public Users(string usrname, string name, string surname, string email, string passwd, int rolId)
+    {
+        this.username = usrname;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.passwd = passwd;
+        this.rolId = rolId;
+    }
 
     public bool checkUser(string username, string password)
     {
@@ -50,12 +62,61 @@ public class Users
             return true;
             
         }
-
-
-
-
         newCon.conn.Close();
 
     }
+
+    public void AddUser()
+    {
+
+        try
+        {
+        Connection newCon = new Connection();
+        string insertSql = "INSERT INTO Member (Username,Name,Surname,Email,roleId,Password) values (@Username,@Name,@Surname,@Email,@RolId,@Password)";
+
+        MySqlCommand cmd = new MySqlCommand();
+        cmd.Connection = newCon.conn;
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = insertSql;
+
+
+        MySqlParameter userName = new MySqlParameter("@Username", MySqlDbType.VarChar, 50);
+        userName.Value = this.username;
+        cmd.Parameters.Add(userName);
+
+        MySqlParameter firstName = new MySqlParameter("@Name", MySqlDbType.VarChar, 120);
+        firstName.Value = this.name;
+        cmd.Parameters.Add(firstName);
+
+        MySqlParameter lastName = new MySqlParameter("@Surname", MySqlDbType.VarChar, 120);
+        lastName.Value = this.surname;
+        cmd.Parameters.Add(lastName);
+
+
+        MySqlParameter email = new MySqlParameter("@Email", MySqlDbType.VarChar, 50);
+        email.Value = this.email;
+        cmd.Parameters.Add(email);
+
+        MySqlParameter pwd = new MySqlParameter("@Password", MySqlDbType.VarChar, 50);
+        pwd.Value = this.passwd;
+        cmd.Parameters.Add(pwd);
+
+        MySqlParameter rolId = new MySqlParameter("@RolId", MySqlDbType.Int32);
+        rolId.Value = this.rolId ;
+        cmd.Parameters.Add(rolId);
+
+        cmd.ExecuteNonQuery();
+        newCon.conn.Close();
+        }
+        catch (MySqlException ex)
+        {
+            string errorMessage = "Error in registring user";
+            errorMessage += ex.Message;
+            throw new Exception(errorMessage);
+
+        }
+   
+    }
+
  
 }
--- Yeni.master.cs
+++ Yeni.master.cs
@@ -9,19 +9,34 @@ public partial class Yeni : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["user"] != null)
+        Page.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        if (!IsPostBack)
         {
-            login_signUp.Visible = false;
-            session_name.Text = ((Users)Session["user"]).name;
-            logout.Visible = true;
+            if (Session["user"] != null)
+            {
+                login_signUp.Visible = false;
+                session_name.Text = ((Users)Session["user"]).name;
+                logout.Visible = true;
+            }
+
+            else
+            {
+                login_signUp.Visible = true;
+                session_name.Text = "Hello Guest!";
+                logout.Visible = false;
+            }
         }
 
         else
         {
-            login_signUp.Visible = true;
-            session_name.Text = "Hello Guest!";
-            logout.Visible = false;
+            Response.ClearHeaders();
+
+            Response.AddHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+
+            Response.AddHeader("Pragma", "no-cache");
+
         }
+
     }
 
 
@@ -33,8 +48,25 @@ public partial class Yeni : System.Web.UI.MasterPage
         if (newUser.checkUser(L_UserName.Text, L_passwd.Text))
         {
             Session.Add("user", newUser);
-            Response.Redirect("HomePage.aspx");
+            Response.Redirect(Request.RawUrl);
         }
         else Alert.Show("Wrong username and password combinations.");
     }
-}
+    protected void Register_Click(object sender, EventArgs e)
+    {
+        if (R_Passwd.Text != R_ConfirmPasswd.Text) Alert.Show("Entered passwords are not matched each other");
+        else
+        {
+            try
+            {
+                Users newUser = new Users(R_Username.Text, FirstName.Text, LastName.Text, Email.Text, R_Passwd.Text, 1);
+                newUser.AddUser();
+                Alert.Show("Registration is successfull");
+            }
+            catch (Exception ex)
+            {
+                Alert.Show(ex.Message);
+            }
+        }
+    }
+ }
