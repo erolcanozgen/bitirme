1aa76dce6ce83e4dba8c86eaf4ce32a708719066
Basri Kaya <basrikaya1@gmail.com>
2014-04-14 21:24:16 +0300

    2-4 tamam

--- Yapılacaklar.cs
+++ Yapılacaklar.cs
@@ -12,16 +12,16 @@ public class Yapılacaklar
 	{
         /**********************************************
          *  1) Disease.aspx de filter'de Min Case-Control Count birleştirilip minumum sample size olacak
-         *  2) Admin page ve meta analizde precision 2 bit hassas olacak
-         *  3) Post Hoc calculator sitedeki gibi olacak
-         *  4) Meta Analiz meta or value olacak
+         * +2) Admin page ve meta analizde precision 2 bit hassas olacak
+         * -3) Post Hoc calculator sitedeki gibi olacak
+         * +4) Meta Analiz meta or value olacak
          *  5) Frekans 0 ise NaN cıkıyor -> Elif Bakacak
-         *  6) Added Pub sayfasına update yazılacak
+         * -6) Added Pub sayfasına update yazılacak
          * +7) Utilities OR-P calculator Experement yerine -> Allele                Allele 1    Allele 2
          *                                                    Number of Cases
          *                                                    Number of Control 
-         *  8) Utilities hardy calculatorde "common homozigotes" -> p2, "heterozigotz" ->q2
-         *  9) Utilities hardy calculator result tablosunda P ve Q küçk olacak(p,q). Xsquare X'2 olacak. P value vermek lazım.
+         * -8) Utilities hardy calculatorde "common homozigotes" -> p2, "heterozigotz" ->q2
+         * -9) Utilities hardy calculator result tablosunda P ve Q küçk olacak(p,q). Xsquare X'2 olacak. P value vermek lazım.
          *  10) Gene isimleri gene card'a gidecek.
          *  11) tablolar güzelleştirilecek - css
          *  12) Add new pub "case allele1 number" ve "control allele1 number" olacak
